f3ec524908b0e316d6d3c04bc182fabc56bdae0e
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2023-01-08 12:12:42 -0500

    Update logs.java

--- logs.java
+++ logs.java
@@ -1,80 +1,62 @@
 /*
-*  This program is based on the Rock-Paper-Scissors game
- *     with a computer opponent.
-*
-* @author  Huzaifa Khalid
-* @version 1.0
-* @since   2023-01-08
+ *
+ * The program finds the maximum run of a string
+ *
+ * @author:  Huzaifa Khalid
+ * @version: 1.0
+ * @since:   2023-1-8
 */
 
 import java.util.Scanner;
 
 /**
- * This function determines the winner between the user and computer.
+* This is the starting of main program.
 */
-final class RockPaperScissors {
-
+public final class Index {
     /**
-    * Prevent instantiation.
-    * Throw an exception IllegalStateException.
-    * if this ever is called
-    *
-    * @throws IllegalStateException
+    * The utility class lint exception rule.
     *
     */
-    private RockPaperScissors() {
-        throw new IllegalStateException("Cannot be instantiated");
+    private Index() {
+        // nothing called
     }
 
+    /**
+    * The main calculation function.
+    *
+    * @param userWord - width number
+    * @return maxRun - int
+    */
+
+    static int maxRunCalculator(final String userWord) {
+        int count = 0;
+        int maxRun = 0;
+        char blankChar = ' ';
+        for (char tempValue : userWord.toCharArray()) {
+            if (tempValue == blankChar) {
+                count++;
+            } else {
+                count = 1;
+                blankChar = tempValue;
+            }
+            if (count > maxRun) {
+                maxRun = count;
+            }
+        }
+        return maxRun;
+    }
     /**
     * The starting main() function.
     *
-    * @param args No args will be used
+    * @param args No args will be used.
     */
-    public static void main(String[] args) {
 
-        final int max = 3;
-        final int min = 1;
-        final int randomNumber = (int) (Math.random() * max + min);
-        final String rock = "rock";
-        final String paper = "paper";
-        final String scissors = "scissors";
-        final String computerDecision = "\nThe computer chose ";
-        final String computer;
-
-        // input
-        final Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter rock, paper, or scissors: ");
-        final String player = userInput.nextLine();
-        // process & output
-        if (randomNumber == 1) {
-            computer = rock;
-        } else if (randomNumber == 2) {
-            computer = paper;
-        } else {
-            computer = scissors;
-        }
-
-        if (player.equals(computer)) {
-            System.out.println(computerDecision + computer);
-            System.out.println("\nTie.");
-        } else if (player.equals(rock) && computer.equals(paper)
-            || player.equals(paper) && computer.equals(scissors)
-            || player.equals(scissors)
-            && computer.equals(rock)) {
-            System.out.println(computerDecision + computer);
-            System.out.println("\nYou lost.");
-        } else if (player.equals(paper) && computer.equals(rock)
-            || player.equals(scissors) && computer.equals(rock)
-            || player.equals(rock)
-            && computer.equals(scissors)) {
-            System.out.println(computerDecision + computer);
-            System.out.println("\nYou win!");
-        } else {
-            System.out.println("\nSomething is not right.");
-        }
+    public static void main(final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        final String userString = sc.nextLine();
+        System.out.printf("\nThe max amount of values in a row is %d",
+                        maxRunCalculator(userString));
         System.out.println("\nDone.");
-
     }
 }
-
